9e9daa9edd03854dfd1a03c83a269fa31cf2d9ab
iLeif96 <ileif@yandex.ru>
2020-11-18 12:29:45 +0300

    AvailablTypes fields changed to static

--- main.cs
+++ main.cs
@@ -16,9 +16,9 @@ namespace AutoCadGcode
 {
     public struct AvailableTypes
     {
-        public Type LINE { get { return typeof(Line); } }
-        public Type ARC { get { return typeof(Arc); } }
-        public Type POLYLINE { get { return typeof(Polyline); } }
+        public static Type LINE { get { return typeof(Line); } }
+        public static Type ARC { get { return typeof(Arc); } }
+        public static Type POLYLINE { get { return typeof(Polyline); } }
     }
 
     public class Global : IExtensionApplication
