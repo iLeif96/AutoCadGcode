c9655b3d754822567703b9688a3bdf96b81760a5
iLeif96 <ileif@yandex.ru>
2020-11-21 13:27:34 +0300

    Class that strictly define subType of command

---
+++ CommandType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCadGcode
+{
+    /// <summary>
+    /// Using for define specific command
+    /// </summary>
+    public enum CommandType { None, DisablePumping, StopAndPump, First, Last }
+}
