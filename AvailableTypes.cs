835910529c8bfc2fe54ac8d228aa4a1575de29b2
iLeif96 <ileif@yandex.ru>
2020-12-09 23:56:47 +0300

    new files

---
+++ AvailableTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AutoCadGcode
+{
+    public struct AvailableTypes
+    {
+        public static Type LINE { get { return typeof(Line); } }
+        public static Type ARC { get { return typeof(Arc); } }
+        public static Type POLYLINE { get { return typeof(Polyline); } }
+    }
+}
---
+++ Global.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace AutoCadGcode
+{
+    public class Global
+    {
+        /// <summary>
+        /// Using for approximate autocad Points coordinates
+        /// </summary>
+        public const double HYSTERESIS = 0.01;
+
+        public static DocumentCollection DocumentManager = Application.DocumentManager;
+
+        public static Editor Editor = Application.DocumentManager.MdiActiveDocument.Editor;
+
+        public static Database DB = Application.DocumentManager.MdiActiveDocument.Database;
+
+        public static Autodesk.AutoCAD.DatabaseServices.TransactionManager TM = DB.TransactionManager;
+
+        public static Document Doc = Application.DocumentManager.MdiActiveDocument;
+    }
+}
